fff73154f183a3d7240e5110b9aedbc4b9d0c2c3
Luke <11898833+curlpipe@users.noreply.github.com>
2024-09-15 21:14:25 +0100

    added data export

--- front_end/src/views/HomeView.vue
+++ front_end/src/views/HomeView.vue
@@ -1,4 +1,73 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { useIngredientStore } from '@/stores/useIngredientStore.js';
+import { useMealStore } from '@/stores/useMealStore.js';
+import { useMealPlanStore } from '@/stores/useMealPlanStore.js';
+import axios from 'axios';
+
+const ingredientStore = useIngredientStore();
+const mealStore = useMealStore();
+const mealPlanStore = useMealPlanStore();
+
+const backup = ref('');
+
+onMounted(async () => {
+    await ingredientStore.query();
+    await mealStore.query();
+    await mealPlanStore.query();
+    await exportData();
+});
+
+const importData = async () => {
+    const data = JSON.parse(backup.value);
+    console.log(data);
+    // Clear the databases
+    // Upload ingredients
+    // Upload meals
+    // Attach ingredients to meals
+    // Upload meal plans
+    // Upload schedule
+};
+
+const exportData = async () => {
+    let result = {};
+    result.ingredients = ingredientStore.getIngredients;
+    result.ingredients.forEach(i => { 
+        delete i['createdAt']; 
+        delete i['updatedAt'];
+    });
+    result.meals = mealStore.getMeals;
+    result.meals.forEach(m => {
+        delete m['createdAt'];
+        delete m['updatedAt'];
+        delete m['explanation'];
+        delete m['score'];
+        m.ingredients.forEach(i => {
+            delete i['createdAt']; 
+            delete i['updatedAt'];
+            delete i['name'];
+            delete i['quantity'];
+            delete i['unit'];
+            i['amount'] = i['MealIngredients'].amount;
+            delete i['MealIngredients'];
+        });
+    });
+    result.plans = mealPlanStore.getMealPlans;
+    result.plans.forEach(p => {
+        delete p['createdAt'];
+        delete p['updatedAt'];
+    });
+    result.schedule = await axios.get('/api/schedule');
+    result.schedule = result.schedule.data;
+    Object.keys(result.schedule).forEach(s => {
+        result.schedule[s].forEach(m => {
+            delete m.createdAt;
+            delete m.updatedAt;
+            delete m.date;
+        });
+    });
+    backup.value = JSON.stringify(result);
+};
 
 </script>
 
@@ -18,4 +87,9 @@
     <RouterLink to="/getcooking"><h5 class="blue-fg">Get Cooking</h5></RouterLink>
     <p style="width: 400px">Get cooking is where you can find the schedule of meals to eat and access the recipe instructions to help guide you as you cook</p>
     <br>
+    <h5>Backup Data</h5>
+    <form style="display: flex; flex-direction: column; gap: 10px;" @submit.prevent="import">
+        <button @click="importData" style="width: 100px;">Import</button>
+        <textarea id="backup" name="backup" v-model="backup" cols="30" rows="30"></textarea>
+    </form>
 </template>
